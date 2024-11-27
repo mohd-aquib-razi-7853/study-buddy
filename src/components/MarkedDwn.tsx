bf566fb46adc555f419d66a908ffe1d6ab92f502
Aq7853gehc <aquib7583@gmail.com>
2024-11-27 11:02:55 +0530

    Solve the errors by removing the files

--- src/components/MarkedDwn.tsx
+++
@@ -1,9 +0,0 @@
-import { marked } from "marked";
-
-const MarkedDwn = ({text}:{text:string})=>{
-  const htmlContent = marked(text)
-  return <div dangerouslySetInnerHTML={}/>
-  
-}
-
-export default MarkedDwn
