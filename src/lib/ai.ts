32609f6a8893cc5869e23a41ba8ea0311443457e
Aquib Ahmed <126333980+Aq7853gehc@users.noreply.github.com>
2025-07-01 17:06:32 +0530

    Update ai.ts

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const API_KEY = process.env.API_KEY
-const genAI = new GoogleGenerativeAI("AIzaSyCqYIUk-6hCSJ-3VrEruebSWzDsfynDKkE");
+const genAI = new GoogleGenerativeAI("AIzaSyB7YhQlkiIF6rlcLGFykiUgtBjWSnt-uOE");
 const model = genAI.getGenerativeModel({ model:  "gemini-2.0-flash" });
 
 const generate_text = async (prompt: string) => {
